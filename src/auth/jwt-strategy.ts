e666b51019ebc82408b7d4fcf5cfc23671de3b30
mr-chidex <mbahchiboy2@gmail.com>
2023-03-30 15:57:43 +0100

    jwtstrategy test added

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -15,7 +15,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey:
+        process.env.NODE_ENV === 'test'
+          ? 'testsecretkey'
+          : process.env.JWT_SECRET,
     });
   }
 
